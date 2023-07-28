8bcbdb70b1dd22bd8415f6ba65fac786395bfefa
melikeucreker <melikeucareker@gmail.com>
2023-07-28 18:14:57 +0300

    melike/2807/locate eklendi

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,6 +1,24 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class HomePage  {
+    public HomePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    //Homepage >ExamResultButton
+    @FindBy(xpath = "//a[normalize-space()='Exam Result']")
+    public WebElement examResultButton;
+
+
+
+
+
+
 
 
 }
