8b63681a58a26db9402cf3c848733f0f66cb49e4
hayyam2395 <ogun.erdogan23@gmail.com>
2023-07-29 11:34:03 +0200

    29.07.2023/ogun-2

--- src/test/java/pages/Base.java
+++ src/test/java/pages/Base.java
@@ -1,2 +1,12 @@
-package pages;public class Base {
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public abstract class Base {
+
+public Base(){
+    PageFactory.initElements(Driver.getDriver(), this);
+}
+
 }
