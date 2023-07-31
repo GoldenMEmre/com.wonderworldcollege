f0b57e857224b4b6ea633f007bb37775cf49eb8b
GultenHarrelson <gulten.harrelson@gmail.com>
2023-07-31 17:26:00 -0500

    Gulten-7-31-V1

--- src/test/java/pages/AdminPage.java
+++ src/test/java/pages/AdminPage.java
@@ -8,7 +8,6 @@ public class AdminPage extends Base {
 
 
     // Admin fees collection section
-
     @FindBy(xpath = "//a[@class='btn btn-lg btn-admin']")
     public WebElement adminLoginButton;
     @FindBy(xpath ="//input[@placeholder='Username']")
@@ -19,13 +18,55 @@ public class AdminPage extends Base {
     public  WebElement signInButton;
     @FindBy(xpath ="//*[text()='Fees Collection']")
     public  WebElement feesCollection;
-
     @FindBy(xpath ="(//*[text()='Fees Master'])[2]" )
     public WebElement feesMasterButton;
-    @FindBy(xpath="(//*[text()='Fees Group'])[3]")
+    @FindBy(xpath = "(//label[@for='exampleInputEmail1'])[1]")
     public WebElement feesGroup;
-    @FindBy(xpath ="//*[@name='feetype_id']")
+    @FindBy(xpath ="(//label[@for='exampleInputEmail1'])[2]")
     public  WebElement feesType;
+    @FindBy(xpath ="(//label[@for='exampleInputEmail1'])[3]")
+    public  WebElement dueDate;
+    @FindBy(xpath = "(//label[@for='exampleInputEmail1'])[4]")
+    public  WebElement percentage;
+    @FindBy(xpath = "(//label[@for='exampleInputEmail1'])[5]")
+    public  WebElement fixAmount;
+    @FindBy(xpath = "(//label[@for='exampleInputEmail1'])[4]")
+    public  WebElement amountButton;
+    @FindBy(xpath = "//label[text()='Fine Type']")
+    public  WebElement fineType;
+
+    @FindBy(xpath =" (//label[@class='radio-inline'])[1]")
+    public  WebElement radioNoneButton;
+    @FindBy(xpath =" (//label[@class='radio-inline'])[2]")
+    public  WebElement radioPercentageButton;
+    @FindBy(xpath =" (//label[@class='radio-inline'])[3]")
+    public  WebElement radioFixButton;
+   @FindBy(xpath ="//input[@placeholder='Search...']")
+    public  WebElement searchBox;
+
+   @FindBy(xpath="//select[@id='fee_groups_id']")
+    public  WebElement feesGroupDropDown;
+   @FindBy(xpath="//select[@id='feetype_id']")
+    public  WebElement feesTypeDropDown;
+   @FindBy(xpath ="//input[@id='due_date']")
+   public  WebElement dueDateDropDown;
+   @FindBy(xpath="//input[@name='amount']")
+    public  WebElement amountDropdown;
+  @FindBy(xpath ="//input[@id='fine_percentage']")
+    public  WebElement percentageSelect;
+  @FindBy(xpath ="//input[@id='fine_amount']")
+    public  WebElement fixAmountSelect;
+   @FindBy(xpath="(//button[@type='submit'])[3]")
+    public  WebElement saveButton;
+   @FindBy(xpath = "Record Saved Successfully")
+    public  WebElement savedSuccessfully;
+   @FindBy(xpath="(//i[@class='fa fa-pencil'])[1]")
+    public  WebElement editButton;
+   @FindBy(xpath="(//i[@class='fa fa-remove'])[1]")
+    public WebElement deleteButton;
+
+
+
 
 
 
--- src/test/java/stepdefinitions/AdminStepdefinition.java
+++ src/test/java/stepdefinitions/AdminStepdefinition.java
@@ -1,11 +1,16 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.Given;
 import org.junit.Assert;
+import org.openqa.selenium.support.ui.Select;
 import pages.AdminPage;
 import pages.Base;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class AdminStepdefinition {
 
@@ -17,7 +22,6 @@ public class AdminStepdefinition {
         adminPage.adminLoginButton.click();
 
        ReusableMethods.switchToWindow("Login : Wonder World College");
-
     }
     @Given("enter username")
     public void enter_username() {
@@ -44,22 +48,93 @@ public class AdminStepdefinition {
         adminPage.feesMasterButton.click();
 
     }
-
     @Given("verify that fees Master  displayed")
     public void verify_that_fees_master_displayed() {
-        Assert.assertTrue(adminPage.feesCollection.isDisplayed());
-
+        assertTrue(adminPage.feesCollection.isDisplayed());
 
     }
     @Given("verify  all title that related with Fees Master are displayed")
     public void verify_all_title_that_related_with_fees_master_are_displayed() {
-        Assert.assertTrue(adminPage.feesGroup.isDisplayed());
+
+        assertTrue(adminPage.feesGroup.isDisplayed());
+        assertTrue(adminPage.feesType.isDisplayed());
+        assertTrue(adminPage.dueDate.isDisplayed());
+        assertTrue(adminPage.percentage.isDisplayed());
+        assertTrue(adminPage.fixAmount.isDisplayed());
+        assertTrue(adminPage.amountButton.isDisplayed());
+        assertTrue(adminPage.fineType.isDisplayed());
+        assertTrue(adminPage.radioNoneButton.isDisplayed());
+        assertTrue(adminPage.radioPercentageButton.isDisplayed());
+        assertTrue(adminPage.radioFixButton.isDisplayed());
+        ReusableMethods.bekle(2);
+        assertTrue(adminPage.searchBox.isEnabled());
+
+    }
+    @Given("verify  all section title that related with Fees Master can be select by clicking")
+    public void verify_all_section_title_that_related_with_fees_master_can_be_select_by_clicking() {
+        Select select=new Select(adminPage.feesGroupDropDown);
+        select.selectByIndex(1);
+        select=new Select(adminPage.feesTypeDropDown);
+        select.selectByIndex(1);
+            adminPage.dueDateDropDown.sendKeys(ConfigReader.getProperty("Date"));
+            ReusableMethods.bekle(2);
+           adminPage.amountDropdown.sendKeys(ConfigReader.getProperty("Amount"));
+           ReusableMethods.bekle(1);
+        adminPage.radioPercentageButton.click();
+       adminPage.percentageSelect.sendKeys(ConfigReader.getProperty("Percentage"));
+       adminPage.fixAmountSelect.click();
+
+    }
+    @Given("Verify Record Saved Successfully displayed after clicking save button")
+    public void verify_record_saved_successfully_displayed_after_clicking_save_button() {
+        adminPage.saveButton.click();
+        String expectedTitle="Record Saved Successfully";
+
+       if(expectedTitle.contains("Record Saved Successfully")){
+           System.out.print("Expected title test Passed");
+
+       }else{
+           System.out.print("Expected title test Failed");
+       }
+    }
+    @Given("Verify that Fees Master List can be updated by clicking edit button")
+    public void verify_that_fees_master_list_can_be_updated_by_clicking_edit_button() {
+        adminPage.editButton.click();
+        Select select=new Select(adminPage.feesGroupDropDown);
+        select.selectByIndex(2);
+        select=new Select(adminPage.feesTypeDropDown);
+        select.selectByIndex(1);
+        adminPage.dueDateDropDown.sendKeys(ConfigReader.getProperty("Date"));
+        ReusableMethods.bekle(2);
+        adminPage.amountDropdown.sendKeys(ConfigReader.getProperty("Amount"));
+        ReusableMethods.bekle(1);
+        adminPage.percentageSelect.sendKeys(ConfigReader.getProperty("Percentage"));
+        adminPage.saveButton.click();
+        adminPage.saveButton.click();
+        String expectedTitle="Record Updated  Successfully";
+
+        if(expectedTitle.contains("Record Updated  Successfully")){
+            System.out.print("Expected title test Passed");
+
+        }else{
+            System.out.print("Expected title test Failed");
+        }
+    }
+    @Given("Verify that the delete button is functional")
+    public void verify_that_the_delete_button_is_functional() {
+    adminPage.deleteButton.click();
+        Driver.getDriver().switchTo().alert().accept();
+    }
+
+
+
+
 
     }
 
 
 
-    }
+
 
 
 
